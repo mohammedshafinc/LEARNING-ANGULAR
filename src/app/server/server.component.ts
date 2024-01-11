ab7c5b9e46de860d443ac69f419b8863ab02cee0
mohammed shafin <shafinms21@gmail.com>
2024-01-11 10:07:10 +0530

    data binding module

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,8 +3,15 @@ import { Component } from "@angular/core";
 @Component({
     selector : 'app-server',
     templateUrl : './server.component.html'
-    
+
 })
 export class ServerComponent{
+    serverId:number = 10
+    serverStatus:string = "offline"
+
+    
+    getSserverStatus(){
+        return this.serverStatus
+    }
 
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,16 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   // selector: '[app-servers]',
   // selector: '.app-servers',
   selector: 'app-servers',
-
-        
-
-
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
-export class ServersComponent {
+export class ServersComponent implements OnInit {
+  allowNewServer = false
+  serverCreationStatus = "server is not Cretead"
+
+
+  constructor(){
+    setTimeout(()=>{
+      this.allowNewServer = true
+    },2000)
+
+  }
+  ngOnInit(): void {
+      
+  }
+  onCreateServer(){
+    this.serverCreationStatus = "server is created"
+  }
 
 }
